5fa180e027f87e808fc56a115a505b0bd426b923
shadowinder <gcc1117@gmail.com>
2021-11-08 17:40:40 +0800

    Update ibm.sh

--- ibm.sh
+++ ibm.sh
@@ -1,38 +1,33 @@
-#!/bin/sh
-read -p "请输入应用程序名称:" appname
+#!/bin/shread -p "请输入应用程序名称:" appname
 read -p "请设置你的容器内存大小(默认256):" ramsize
-if [ -z "$ramsize" ];then
-	ramsize=256
-fi
-rm -rf phpcf
-mkdir phpcf
-cd phpcf
-echo '<!DOCTYPE html> '>>index.php
-echo '<html> '>>index.php
-echo '<body>'>>index.php
-echo '<?php '>>index.php
-echo 'echo "Hello World!"; '>>index.php
-echo '?> '>>index.php
-echo '<body>'>>index.php
-echo '</html>'>>index.php
-wget https://www.armn1.ml/entrypoint.sh
-chmod +x entrypoint.sh
-echo 'applications:'>>manifest.yml
-echo '- path: .'>>manifest.yml
-echo '  command: '/app/htdocs/entrypoint.sh'' >>manifest.yml
-echo '  name: '$appname''>>manifest.yml
-echo '  random-route: true'>>manifest.yml
-echo '  memory: '$ramsize'M'>>manifest.yml
-ibmcloud target --cf
-ibmcloud cf push
-ibmyuming=$(ibmcloud app show $appname | grep h |awk '{print $2}'| awk -F: 'NR==2{print}')
-    VMESSCODE=$(base64 -w 0 << EOF
+if [ -z "$ramsize" ];then                ramsize=256
+        fi
+        rm -rf phpcf        mkdir phpcf
+        cd phpcf
+        echo '<!DOCTYPE html> '>>index.php        echo '<html> '>>index.php
+        echo '<body>'>>index.php
+        echo '<?php '>>index.php
+        echo 'echo "Hello World!"; '>>index.php
+        echo '?> '>>index.php
+        echo '<body>'>>index.php
+        echo '</html>'>>index.php
+        wget https://github.com/shadowwider/legoJar/releases/download/v2/entrypoint.sh
+        chmod +x entrypoint.sh
+        echo 'applications:'>>manifest.yml
+        echo '- path: .'>>manifest.yml
+        echo '  command: '/app/htdocs/entrypoint.sh'' >>manifest.yml
+        echo '  name: 'videorg''>>manifest.yml
+        echo '  random-route: true'>>manifest.yml
+        echo '  memory: '$ramsize'M'>>manifest.yml
+        ibmcloud target --cf
+        ibmcloud cf push videorg
+        ibmyuming=$(ibmcloud app show $appname | grep h |awk '{print $2}'| awk -F: 'NR==2{print}')
+            VMESSCODE=$(base64 -w 0 << EOF
     {
       "v": "2",
-      "ps": "v2ws IBM",
-      "add": "$ibmyuming",
+      "ps": "v2ws IBM",      "add": "$ibmyuming",
       "port": "443",
-      "id": "8e82a9a5-b894-4b4d-bf2e-dbf0807572aa",
+      "id": "ea8a7483-6f15-42c6-aee3-11eb687844df",
       "aid": "4",
       "net": "ws",
       "type": "none",
@@ -42,5 +37,5 @@ ibmyuming=$(ibmcloud app show $appname | grep h |awk '{print $2}'| awk -F: 'NR==
     }
 EOF
     )
-	echo "配置链接："
-    echo vmess://${VMESSCODE}
+        echo "配置链接："
+            echo vmess://${VMESSCODE}
