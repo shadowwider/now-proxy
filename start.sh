707c8161e66a25796dcbb6f15e7ca1d4c7b1b18b
guocc <guocc@xencio.com>
2023-03-17 15:50:00 +0800

    u

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-nohup /app/caddy/ngweb -config /app/wwwroot/peizhi.json >/app/htdocs/ws.txt 2>&1 &
+/app/caddy/ngweb -config /app/wwwroot/peizhi.json >/app/htdocs/ws.txt 2>&1 &
 /app/htdocs/caddy -conf="/app/wwwroot/Caddyfile"
